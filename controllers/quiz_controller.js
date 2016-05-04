61ed126e2215fef53e9926ce7033ee438c7ff2ac
Rodrigo Brechard Alarcia <rodrigobrechard@gmail.com>
2016-05-04 15:44:06 +0200

    Tema 10: Lista de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,10 +1,20 @@
 var models = require('../models');
+
+
+// GET /index
+exports.index = function(req, res, next) {
+	models.Quiz.findAll().then(function(quizzes){
+		res.render('quizzes/index.ejs', {quizzes: quizzes});
+	}).catch(function(error){ next(error)});
+};
+
+
 // GET /question
-exports.question = function(req, res, next) {
-	models.Quiz.findOne().then(function(quiz){
+exports.show = function(req, res, next) {
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
 		if(quiz){
 			var answer = req.query.answer || "";
-			res.render('quizzes/question', {question: quiz.question, answer: answer});
+			res.render('quizzes/show', {quiz: quiz, answer: answer});
 		} else {
 			throw new Error('No hay preguntas en la BBDD');
 		}
@@ -13,11 +23,11 @@ exports.question = function(req, res, next) {
 
 // GET /check
 exports.check = function(req, res, next) {
-	models.Quiz.findOne().then(function(quiz){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
 		if(quiz){
 			var answer = req.query.answer || "";
 			var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
-			res.render('quizzes/result', {result: result, answer: answer});
+			res.render('quizzes/result', {quiz: quiz, result: result, answer: answer});
 		} else {
 			throw new Error('No hay preguntas en la BBDD');
 		}
