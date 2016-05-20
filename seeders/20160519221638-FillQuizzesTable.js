a3b37aa50fd9b73915b749b81b2986f0a5cb92e9
Rodrigo Brechard Alarcia <rodrigobrechard@gmail.com>
2016-05-20 16:40:12 +0200

    Arreglos

--- seeders/20160519221638-FillQuizzesTable.js
+++ seeders/20160519221638-FillQuizzesTable.js
@@ -5,7 +5,7 @@ module.exports = {
       return queryInterface.bulkInsert('Quizzes', [
         { question: 'Capital de Italia', answer: 'Roma', createdAt: new Date(), updatedAt: new Date() },
         { question: 'Capital de Portugal', answer: 'Lisboa', createdAt: new Date(), updatedAt: new Date() },
-        { question: 'Capital de España', answer: 'Madrid', createdAt: new Date(), updatedAt: new Date() },
+        { question: 'Capital de España', answer: 'Madrid', createdAt: new Date(), updatedAt: new Date() }
       ]);
   },
   down: function (queryInterface, Sequelize) {
